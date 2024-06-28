d048e03321793fae6b5b46aa2184c31fd2707664
Jorge Salas <jsalopdev@gmail.com>
2024-06-28 10:28:33 -0500

    src

--- src/components/AllReservas.js
+++ src/components/AllReservas.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axiosInstance from '../axiosConfig';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './CustomNavbar';
-import { Modal, Spinner } from 'react-bootstrap';
-import './ReservaList.css'; // Asegúrate de importar los estilos personalizados
 
 const AllReservas = () => {
     const [reservas, setReservas] = useState([]);
@@ -32,6 +30,14 @@ const AllReservas = () => {
         return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
     }, []);
 
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>Error: {error.message}</p>;
+    }
+
     return (
         <div>
             <CustomNavbar />
@@ -58,11 +64,6 @@ const AllReservas = () => {
                     </tbody>
                 </table>
             </div>
-            <Modal show={loading} centered contentClassName="loading-modal">
-                <Modal.Body className="d-flex justify-content-center align-items-center">
-                    <Spinner animation="border" role="status" />
-                </Modal.Body>
-            </Modal>
         </div>
     );
 };
