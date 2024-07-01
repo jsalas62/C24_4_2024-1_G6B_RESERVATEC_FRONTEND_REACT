577749f40f20f3522006679ca7a331c569180f73
Oliver Mendez <mendezoliver2108@gmail.com>
2024-06-30 22:48:33 -0500

    logout

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -4,17 +4,17 @@ import { useNavigate } from 'react-router-dom';
 
 const LogoutButton = () => {
     
-    const handleLogout = async () => {
-        try {
-            await axiosInstance.post('/logout');
-            alert('Logout exitoso'); // Puedes mostrar un mensaje de confirmación si lo deseas
-            window.location.href = 'http://localhost:3000';
-        } catch (error) {
-            console.error('Error al realizar logout:', error);
-            alert('Error al intentar cerrar sesión. Inténtalo de nuevo.'); // Manejo básico de errores
+    // const handleLogout = async () => {
+    //     try {
+    //         await axiosInstance.post('/logout');
+    //         alert('Logout exitoso'); // Puedes mostrar un mensaje de confirmación si lo deseas
+    //         window.location.href = 'http://localhost:3000';
+    //     } catch (error) {
+    //         console.error('Error al realizar logout:', error);
+    //         alert('Error al intentar cerrar sesión. Inténtalo de nuevo.'); // Manejo básico de errores
 
-        }
-    };
+    //     }
+    // };
 
     const navigate = useNavigate();
     
