23dc3354ab239d87c4eac4bdaeb7af104cbf71f8
Oliver Mendez <mendezoliver2108@gmail.com>
2024-07-01 03:27:22 -0500

    input

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -117,11 +117,12 @@ const Profile = () => {
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="carreraId" className="block text-sm font-semibold leading-6 text-gray-900">Carrera</label>
-                            <div className="mt-2.5">
-                                {carreras.length > 0 && (
-                                    <p>{carreras[0].nombre}</p>
-                                )}
-                            </div>
+                            <div className="mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ">
+                          {carreras.length > 0 && (
+                              <p className="text-lg font-semibold">{carreras[0].nombre}</p>
+                          )}
+                          </div>
+
                         </div>
                     </div>
                 </form>
